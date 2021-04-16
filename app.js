9b9caa3a77fa5b4efbcbc80d044e85ca3c042810
Justin Law <justinlaw31@gmail.com>
2021-04-15 22:26:00 -0700

    Responsive + scroll-snap feature

--- app.js
+++ app.js
@@ -1,40 +1,20 @@
 const logo = document.getElementById('logo');
 const heroImg = document.getElementById('heroImg');
 const heroHeading = document.getElementById('heroHeading');
-const heroText = document.getElementById('heroText');
-const heroBtn = document.getElementById('heroBtn');
-const navList = document.getElementById('navList');
-const dlIcon = document.getElementById('dlIcon');
+const heroHeading2 = document.getElementById('heroHeading2');
 
 //function for changes on resize
-// function renderHeroElements() {
-//   if (window.innerWidth > 1024) {
-//     logo.style.opacity = 1;
-//     navList.style.opacity = 1;
-//     heroHeading.textContent = 'instock program';
-//     heroText.innerHTML = `<span class="hero__text--d">d</span> series
-//     <span class="hero--margin-left">by</span>
-//     <span class="hero__text--name">Carl Gustav Magnusson</span>`;
-//     dlIcon.style.display = 'none';
-//   } else if (window.innerWidth > 480 && window.innerWidth <= 1024) {
-//     logo.style.opacity = 1;
-//     navList.style.opacity = 0;
-//     heroHeading.textContent = 'an instock solution coming soon';
-//     heroText.textContent = 'new website is under construction';
-//     dlIcon.style.display = 'none';
-//   } else if (window.innerWidth <= 480) {
-//     logo.style.opacity = 0;
-//     navList.style.opacity = 1;
-//     heroHeading.textContent = '';
-//     heroText.textContent = '';
-//     dlIcon.style.display = 'block';
-//   }
-// }
+function renderHeroElements() {
+  if (window.innerWidth < 1440) {
+    heroHeading.textContent = 'an instock solution coming soon';
+    heroHeading2.textContent = 'an instock solution coming soon';
+  } else {
+    heroHeading.textContent = 'instock solution coming soon';
+    heroHeading2.textContent = 'instock solution coming soon';
+  }
+}
 
-// //initial load of hero
-// renderHeroElements();
+//initial load of hero
+renderHeroElements();
 
-//if window size is desktop
-//tablet
-//phone
-// window.addEventListener('resize', renderHeroElements);
+window.addEventListener('resize', renderHeroElements);
